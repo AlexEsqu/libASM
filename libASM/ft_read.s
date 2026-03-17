9a744d071fcad38215ad83a4385d17b6c22d00a4
Alex <momiji.kling@gmail.com>
2026-03-17 11:18:43 +0100

    asm read cannot be that simple

--- libASM/ft_read.s
+++ libASM/ft_read.s
@@ -1,5 +1,12 @@
 global ft_read
 
 ft_read:
-	MOV rax,1	; set
-	MOV rdi,
+	; GET ARGUMENTS
+	; use GCC calling convention stating non float arguments are stored in rdi, rsi, rdx, rcx, r8, r9, then the stack
+					; ARG 1 - keep file descriptor into register Destination Index (RDI)
+					; ARG 2 - keep destination buffer into register Source Index (RSI)
+					; ARG 3 - keep byte count in register D (RDX)
+	; since the read() libc function already fits the GCC calling convention
+
+	MOV rax,0	; set the syscall code for sys_read
+	SYSCALL		; call syscall using the arguments set into register A, D, SI and DI
